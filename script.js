3a9884d7318e7d604058fc389d78e52359f4ef01
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-11 09:16:54 -0700

    Refactored code, removed unnecessary comments

--- script.js
+++ script.js
@@ -7,63 +7,53 @@ const upLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const specialChars = '!@#$%^&*';
 const lowLetters = upLetters.toLowerCase();
 
+// Collect password criteria through window prompts and generate a randomized string
 function generatePassword() {
   var passwordString = '';
-
-  // const pwLength = window.prompt('Please indicate the number of characters for your password. \
-  // Password must have a minimum of 8 characters but no more than 128 characters.');
-  // const requestNum = window.prompt('Would you like numbers in your password?');
-  // const requesUpCase = window.prompt('Would you like uppercase letters?');
-  // const requestSpecial = window.prompt('Would you like special characters?');
   
-  const pwLength = window.prompt('Please indicate the number of characters for your password. \
+  // Password length
+  const pwLength = prompt('Please indicate the number of characters for your password. \
   Password must have a minimum of 8 characters but no more than 128 characters.');
   if (pwLength >= 8 && pwLength <= 128) {
     var passLength = pwLength;
     passwordString += lowLetters;
   } else {
-    window.alert('Please input a number between 8 and 128.');
+    alert('Please input a number between 8 and 128.');
     return;
   }
 
-  const requestNum = window.confirm('Would you like numbers in your password?');
+  // Numeric characters
+  const requestNum = confirm('Would you like numbers in your password?');
   if (requestNum) {
-  // if (options.includes(requestNum.toLowerCase())) {
     passwordString += numChars;
-    // var updateNums = password.concat(numChars);
   } else {
-    window.alert("Your password will not contain numbers.");
+    alert("Your password will not contain numbers.");
   }
-  // return passwordString;
 
-  const requesUpCase = window.confirm('Would you like uppercase letters?');
-  // if (options.includes(requesUpCase.toLowerCase())) {
+  // Uppercase characters
+  const requesUpCase = confirm('Would you like uppercase letters?');
     if (requesUpCase) {
     passwordString += upLetters;
-    // var updateLetterUp = password.concat(upLetters);
   } else {
-    window.alert("Your password will not contain uppercase letters.");
+    alert("Your password will not contain uppercase letters.");
   }
-  // return passwordString;
 
-  const requestSpecial = window.confirm('Would you like special characters?');
+  // Special characters
+  const requestSpecial = confirm('Would you like special characters?');
   if (requestSpecial) {
-  // if (options.includes(requestSpecial.toLowerCase())) {
     passwordString += specialChars;
-    //  var updateSpecial = password.concat(specialChars);
   } else {
-    window.alert("Your password will not contain special characters.");
+    alert("Your password will not contain special characters.");
   }
-  // return passwordString;
 
+  // From passwordString, generate a random password
   var password = '';
   for (let i = 0; i <= passLength - 1; i++) {
     var randomizePass = Math.floor(Math.random()*passwordString.length);
     password += passwordString.charAt(randomizePass);
   }
-
   return password;
-  
+
 }
 
 // Write password to the #password input
